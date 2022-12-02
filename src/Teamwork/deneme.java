612fbdb9ae7add65a48f1083ca4ab1b9da8bddb6
grsy-ulucam <emre.yucel48.45@gmail.com>
2022-12-03 01:35:53 +0300

    ilk commit

---
+++ src/Teamwork/deneme.java
@@ -0,0 +1,14 @@
+package Teamwork;
+
+import java.nio.file.Files;
+
+public class deneme {
+    public static void main(String[] args) {
+   int a=12;
+   int b=25;
+        System.out.println(a+b);
+
+
+
+    }
+}
