3de191bd1606b09f361014b249b58e24cedc2da6
grsy-ulucam <emre.yucel48.45@gmail.com>
2022-12-03 02:08:30 +0300

    ilk commit

---
+++ src/Teamwork/deneme2.java
@@ -0,0 +1,11 @@
+package Teamwork;
+
+public class deneme2 {
+    public static void main(String[] args) {
+        String a="ali";
+        String b="kaçar";
+        System.out.println("a.contains(b) = " + a.contains(b));
+        System.out.println(b.contains(a));
+
+    }
+}
