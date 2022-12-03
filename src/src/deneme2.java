e8e718a82f501c0daae2a9be0b74c183a49dd6ae
grsy-ulucam <emre.yucel48.45@gmail.com>
2022-12-03 16:44:30 +0300

    yeni dosya

---
+++ src/src/deneme2.java
@@ -0,0 +1,19 @@
+package src;
+
+public class deneme2 {
+    public static void main(String[] args) {
+
+        int a=12;
+        int b=10;
+        System.out.println(a+b);
+
+        int c=12;
+        int d=10;
+        System.out.println(d+c);
+
+        int e=12;
+        int f=10;
+        System.out.println(e+f);
+
+    }
+}
