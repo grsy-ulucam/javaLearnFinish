2a0b701186bc7d04282a08beeb496ab385826121
grsy-ulucam <emre.yucel48.45@gmail.com>
2022-12-03 16:34:53 +0300

    fist

---
+++ src/src/deneme1.java
@@ -0,0 +1,17 @@
+package src;
+
+public class deneme1 {
+    public static void main(String[] args) {
+
+        int a=12;
+        int b=10;
+        System.out.println(a+b);
+
+        int c=12;
+        int d=10;
+        System.out.println(d+c);
+
+
+
+    }
+}
