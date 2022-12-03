7d77df9256497caa220e0d288b6be2fa8c40d801
grsy-ulucam <emre.yucel48.45@gmail.com>
2022-12-03 18:20:27 +0300

    fist commit

---
+++ src/src/deneme3.java
@@ -0,0 +1,18 @@
+package src;
+
+public class deneme3 {
+    public static void main(String[] args) {
+
+
+        int x=10;
+        int y=12;
+
+        System.out.println(x+y);
+
+
+    }
+
+
+
+
+}
