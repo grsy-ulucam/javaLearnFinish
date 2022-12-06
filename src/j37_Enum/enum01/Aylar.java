3734943960fd7719bad11b21f5da6d2608450695
grsy-ulucam <emre.yucel48.45@gmail.com>
2022-12-06 19:30:07 +0300

    first commit

---
+++ src/j37_Enum/enum01/Aylar.java
@@ -0,0 +1,21 @@
+package j37_Enum.enum01;
+
+public enum Aylar {
+    OCAK,
+    ŞUBAT,
+    MART,
+    NİSAN,
+    MAYIS,
+    HAZİRAN,
+    TEMMUZ,
+    AGUSTOS,
+    EYLUL,
+    EKİM,
+    KASIM,
+    ARALIK,
+
+
+
+
+
+}
